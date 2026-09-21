f06cadf6f01a1cbaed560e69a536ae1a519ee824
sergio.moreira <49799640+moreira-dev@users.noreply.github.com>
2026-09-21 11:54:37 +1000

    New: Extracts lines of text from text-based PDFs for further processing

---
+++ src/lib/types/index.ts
@@ -0,0 +1,41 @@
+export type TransformMatrix = {
+	scaleX: number,
+	skewY: number,
+	skewX: number,
+	scaleY: number,
+	translateX: number,
+	translateY: number
+};
+
+export interface TextPiece {
+	text: string;
+	start: number;
+	transform: TransformMatrix;
+	width: number;
+	fontName: string;
+	fontFamily: string;
+	fontSize: number;
+}
+
+export interface TextLine {
+	pageNumber: number;
+	text: string;
+	pieces: TextPiece[];
+}
+
+export interface Detection {
+	pageNumber: number;
+	lineIndex: number;
+	start: number;
+	end: number;
+	label: string;
+	text: string;
+}
+
+export interface Box {
+	pageNumber: number;
+	x: number;
+	y: number;
+	width: number;
+	height: number;
+}
